9d462fd5e53915dfff29e2c1c36b1d2fc5d9fff4
Shurpanchik <Shur1993@mail.ru>
2017-08-25 15:02:57 +0300

    добавлен драйвер и пересоздание драйвера для нового теста

--- src/main/java/helpers/Driver.java
+++ src/main/java/helpers/Driver.java
@@ -10,16 +10,15 @@ public class Driver {
 
     public static synchronized WebDriver getInstance() {
         if (driver == null) {
-            // Как запустить тесты из firefox под selenium v3
-            //http://learn-automation.com/use-firefox-selenium-using-geckodriver-selenium-3/
-            System.setProperty("webdriver.gecko.driver","src\\main\\resources\\geckodriver.exe");
-
             updateDriver();
         }
         return driver;
     }
 
     public static void updateDriver() {
+        // Как запустить тесты из firefox под selenium v3
+        //http://learn-automation.com/use-firefox-selenium-using-geckodriver-selenium-3/
+        System.setProperty("webdriver.gecko.driver","src\\main\\resources\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
