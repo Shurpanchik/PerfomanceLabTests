2d8162f5070e13ba4dc50c0293c47e1046753fa5
Татьяна Шур <tshur@webzavod.ru>
2017-08-27 21:55:10 +0300

    тесты перенесены

--- src/test/java/SearchPerfomanceLabTest.java
+++ src/test/java/SearchPerfomanceLabTest.java
@@ -1,21 +1,13 @@
-package tests;
 
-import helpers.Driver;
-import org.openqa.selenium.WebDriver;
-import pages.perfomancelabsite.pages.AvtomatizacijaTestirovanijaPage;
-import pages.perfomancelabsite.pages.SoftwareTestingPage;
-import pages.perfomancelabsite.top.SubTopServices;
+
 import pages.searchsites.GoogleSearch;
 import pages.searchsites.RamblerSearch;
-import pages.searchsites.SearchPage;
 import pages.searchsites.YandexSearch;
 import org.junit.Test;
-import steps.PerfomanceLabSteps;
-import steps.SearchSteps;
 
 import static org.junit.Assert.fail;
 
-public class SearchPerfomanceLabTest extends TestSignatur {
+public class SearchPerfomanceLabTest extends tests.TestSignatur {
 
     @Test
     public void SearchYandex() throws InterruptedException {
